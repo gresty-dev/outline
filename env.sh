0ba1e71109960768e06546a2ceabd069f6c0315a
Chris Gresty <chris@gresty.dev>
2024-02-25 16:04:34 +0000

    Upgrade to v0.75.2

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export IMAGE=outline
-export IMAGE_VERSION=0.75.0
+export IMAGE_VERSION=0.75.2
 export SUFFIX=2tudor
 export REGISTRY=docker.2tudor.net
 export CA_ROOT=root_ca-2tudor.net.crt
