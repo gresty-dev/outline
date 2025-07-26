1fbbd603d067a9e159dd9ba627fea6aefc3d7ed6
Chris Gresty <chris@gresty.dev>
2025-07-26 11:41:39 +0100

    upgrade outline to 0.85.1

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export IMAGE=outline
-export IMAGE_VERSION=0.78.0
+export IMAGE_VERSION=0.85.1
 export SUFFIX=2tudor
 export REGISTRY=docker.2tudor.net
 export CA_ROOT=root_ca-2tudor.net.crt
